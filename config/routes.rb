1381c7e042c39944d91f0e58df641a80c19d3b67
devestic <andreas.nygren.webbutvecklare@gmail.com>
2020-09-27 15:27:36 +0200

    Skapade routes för Create, Read, Update och Delete i filen routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api do
+    #Tar höjd för framtida förändringar med versionshanteringen
+    namespace :v1 do
+      #resources gör det möjligt att samla önskvärda actions på samma rad
+      resources :coffe, only: [:index, :create, :destroy, :update]
+    end
+  end
+  
+
 end
